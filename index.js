5adb5fde71a8def3f8f0e44a1a332eb71fe87f3b
Erik Hofer <erik.hofer@bmiag.de>
2023-05-26 10:13:57 +0200

    feat: add fictional colors

---
+++ index.js
@@ -0,0 +1,44 @@
+module.exports = {
+  colors: [
+    {
+      "name": "Crimson Luster",
+      "code": "#A30021"
+    },
+    {
+      "name": "Celestial Azure",
+      "code": "#0047AB"
+    },
+    {
+      "name": "Gilded Emerald",
+      "code": "#347F39"
+    },
+    {
+      "name": "Aetherial Lavender",
+      "code": "#9B7FD1"
+    },
+    {
+      "name": "Radiant Coral",
+      "code": "#FF6961"
+    },
+    {
+      "name": "Mystic Obsidian",
+      "code": "#120F16"
+    },
+    {
+      "name": "Luminescent Gold",
+      "code": "#FFD700"
+    },
+    {
+      "name": "Verdant Oasis",
+      "code": "#008080"
+    },
+    {
+      "name": "Sable Rose",
+      "code": "#90303C"
+    },
+    {
+      "name": "Iridescent Pearl",
+      "code": "#EAEAEA"
+    }
+  ]  
+}
